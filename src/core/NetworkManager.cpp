618a8433b4083fa98a0e2a9a647c268c8214df78
kirin2461 <83828028+kirin2461@users.noreply.github.com>
2026-02-12 15:56:20 +0300

    Fix NetworkManager.cpp: define _WIN32_WINNT 0x0600 for MIB_IF_ROW2 and GetIfEntry2 on MSVC

--- src/core/NetworkManager.cpp
+++ src/core/NetworkManager.cpp
@@ -6,6 +6,12 @@
 #include <cstdio>
 
 #ifdef _WIN32
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0600
+#endif
+#endif
+
+#ifdef _WIN32
 #include <winsock2.h>
 #include <iphlpapi.h>
 #include <ws2tcpip.h>
